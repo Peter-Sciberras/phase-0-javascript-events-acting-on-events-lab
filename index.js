23202d0709fb5ef459a1578ef80899ffd1205f10
Peter-Sciberras <sciberrasp@yahoo.com.au>
2023-02-11 20:51:17 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+// moveDodgerLeft function
+function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    let left = parseInt(dodger.style.left);
+    if (left > 0) {
+      dodger.style.left = `${left - 5}px`;
+    }
+  }
+
+  // moveDodgerRight function
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    let left = parseInt(dodger.style.left);
+    if (left < 360) {
+      dodger.style.left = `${left + 5}px`;
+    }
+  }
+
+  // tying left and right arrow key events
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
